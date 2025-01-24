8c3b681a23d41996887277519cbd796005b7da16
rogervdo <rogervdo@icloud.com>
2025-01-23 20:18:16 -0600

    Add implementation of a priority queue using a doubly linked list

--- Octubre/08_pilasfilaspriority.cpp
+++ Octubre/08_pilasfilaspriority.cpp
@@ -165,6 +165,102 @@ void Queue::show() {
     }
 }
 
+class pQueue {
+private:
+    dNode* front;
+    dNode* end;
+    int size;
+
+public:
+    pQueue() {
+        size = 0;
+        front = end = nullptr;
+    }
+
+    ~pQueue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+
+    bool enqueue(int newValue);
+    int dequeue();
+    int getFront();
+    bool isEmpty() { return size == 0; }
+    int getSize() { return size; }
+    void show();
+};
+
+bool pQueue::enqueue(int newValue) {
+
+    dNode* newNode = new dNode(newValue);
+
+    if (isEmpty()) {
+        front = end = newNode;
+        size++;
+        return true;
+    }
+
+    dNode* aux = front;
+    while (aux != nullptr && aux->data <= newValue) {
+        aux = aux->next; // stops on a nullptr, or 1 value higher than input.
+    }
+
+    if (aux == front) { // if no nodes exist then insert at front
+        newNode->next = front;
+        front->prev = newNode;
+        front = newNode;
+    }
+    else if (aux = nullptr) { // else if node is null it means its the end node
+        newNode->prev = end;
+        end->next = newNode;
+        end = newNode;
+    }
+    else { // 
+        newNode->next = aux; // assigns newNode pointers
+        newNode->prev = aux->prev;
+
+        (aux->prev)->next = newNode; // points to newNode
+        aux->prev = newNode;
+    }
+
+    size++;
+    return true;
+}
+
+int pQueue::dequeue() {
+    if (isEmpty()) {
+        std::cout << "Cola vacia, no se puede quitar.\n";
+        return -1;
+    }
+
+    int dequeueData = front->data; // Extraemos data de front
+    dNode* temp = front;
+    front = front->next; // y pasamos front al siguiente.
+
+    if (front != nullptr) { front->prev = nullptr; } // si hay mas valores, designamos que el nuevo front no tiene previo
+    else { end = nullptr; } // significa que la lista esta vacia, y end tambien es nullptr;
+
+    delete temp;
+    size--;
+    return dequeueData;
+}
+
+int pQueue::getFront() {
+    if (isEmpty()) {
+        std::cout << "getFront: Cola vacia, no existe\n";
+        return -1;
+    }
+    return front->data;
+}
+
+void pQueue::show() {
+    dNode* aux = front;
+    while (aux != nullptr) {
+        std::cout << "pQueue: " << aux->data << std::endl;
+        aux = aux->next;
+    }
+}
 int main() {
     std::cout << "Stack Section\n";
 
@@ -198,4 +294,6 @@ int main() {
 
     std::cout << "\nQueue size: " << queue->getSize() << "\n";
     queue->show();
+
+    return 0;
 }
